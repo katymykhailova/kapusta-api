9a160e50036adbbcf6c09f32b5d392d0eb7203bb
Kateryna Mykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-11-04 14:11:34 +0200

    change server

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,14 @@
-const app = require('../app')
+const mGoose = require('mongoose');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3000
+const app = require('../app');
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mGoose
+  .connect(DB_HOST)
+  .then(() => app.listen(PORT))
+  .catch(e => {
+    console.log(e.message);
+    process.exit(1);
+  });
