0a325b8f1d6e808f7682b4a50212622f56c690c2
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-11-04 18:14:24 +0200

    something for login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,7 +13,7 @@ router.post(
   validation(yupUserSchema),
   controllerWrapper(ctrl.signup),
 );
-// router.post('/login', validation(joiUserSchema), controllerWrapper(ctrl.login));
+router.post('/login', validation(yupUserSchema), controllerWrapper(ctrl.login));
 // router.get('/logout', authenticate, controllerWrapper(ctrl.logout));
 // router.get('/current', authenticate, controllerWrapper(ctrl.getCurrentUser));
 module.exports = router;
