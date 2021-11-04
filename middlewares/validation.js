2596008b3884cfd0ea20bb87ea69cdddc1b36ffc
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-11-04 18:40:08 +0200

    cheked login

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,10 +1,10 @@
-const validation = (schema) => {
+const validation = schema => {
   return async (req, res, next) => {
     try {
       await schema.validate(req.body);
     } catch (error) {
       res.status(400).json({
-        status: "error",
+        status: 'error',
         code: 400,
         message: `Error ${error.path} - ${error.errors}`,
       });
