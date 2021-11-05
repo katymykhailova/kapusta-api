9fa5645c07b87495b89c245070909275c6d94bc1
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-11-05 18:44:41 +0200

    something for google

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/auth');
+const {
+  googleAuth,
+  googleRedirect,
+} = require('../../controllers/auth/googleAuth');
 const { yupUserSchema } = require('../../models/user');
 const {
   controllerWrapper,
@@ -15,5 +19,9 @@ router.post(
 );
 router.post('/login', validation(yupUserSchema), controllerWrapper(ctrl.login));
 router.get('/logout', authenticate, controllerWrapper(ctrl.logout));
+
+router.get('/google', controllerWrapper(googleAuth));
+
+router.get('/google-redirect', controllerWrapper(googleRedirect));
 
 module.exports = router;
