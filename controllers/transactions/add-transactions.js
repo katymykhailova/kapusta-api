32aa4716dd40c601fe965dc50e1385a61252ec05
Kateryna Mykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-11-06 16:22:28 +0200

    add post

---
+++ controllers/transactions/add-transactions.js
@@ -0,0 +1,13 @@
+const { sendSuccessRes } = require('../../helpers');
+const { Transaction } = require('../../models');
+// const { Conflict } = require('http-errors');
+
+const addTransaction = async (req, res) => {
+  // const { _id } = req.user;
+
+  // const result = await Transaction.create({ ...req.body, owner: _id });
+  const result = await Transaction.create({ ...req.body });
+  sendSuccessRes(res, { result }, 201);
+};
+
+module.exports = addTransaction;
