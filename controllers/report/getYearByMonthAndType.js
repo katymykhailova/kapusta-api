eec44762eb953641d2728d08de73cf14209a1949
Shuydo <shuydo12@gmail.com>
2021-11-08 19:16:24 +0200

    wash $ go

--- controllers/report/getYearByMonthAndType.js
+++ controllers/report/getYearByMonthAndType.js
@@ -1,18 +1,11 @@
 // for ex: http://localhost:3000/api/transactions/i-2021 ('i[nput]-доход, [o]utput-расход:')
 const { sendSuccessRes } = require('../../helpers');
 const { Transaction } = require('../../models');
-
-console.log("in getYearByMonthAndType file");
-
-const getYearByMonthAndType = async (req, res) => {
-
-  console.log("in getYearByMonthAndType");
-
-  // console.log("i/o:",Object.keys(req.query)[0]);
+const getYearByMonthAndType = async (req, res) => {  // console.log("in getYearByMonthAndType");  // console.log("i/o:",Object.keys(req.query)[0]);
   const key = Object.keys(req.query)[0].toLowerCase();
   let typeTrAct; // получение типа искомых тАкций из запроса
   if (key === 'i' || key === 'o') typeTrAct = key === 'i' ? true : false;
-  else console.log('Error key !!!'); // if (typeTrAct) console.log('Доход');  else console.log('Расход');
+  else console.log('Error type !!!'); // if (typeTrAct) console.log('Доход');  else console.log('Расход');
 
   const query = Object.values(req.query)[0];
   const year = query.substr(0, 4); // получение 4х цифр года из запроса, с отбрасыванием остального
@@ -20,7 +13,6 @@ const getYearByMonthAndType = async (req, res) => {
   const result = await Transaction.find(
     {
       type: typeTrAct,
-      // owner: '61879723dcf34c690cabb318',
       owner:req.user._id,
       date: { $gte: year, $lt: toDate },
     },
@@ -36,33 +28,29 @@ const getYearByMonthAndType = async (req, res) => {
   
 // ============ debug block =================
 // удалить и раскоментировать строку за ним
-  sums.forEach((monthSum, monthNum) => console.log(monthNum, '-', monthSum));
-  console.log("req:",req.user._id);
-  const debugResult = await Transaction.find(
-    {
-      // фильтация (без полей - все)
-      type: typeTrAct,
-      // owner: '61879723dcf34c690cabb318',
-      owner:req.user._id,
+  // sums.forEach((monthSum, monthNum) => console.log(monthNum, '-', monthSum));
+  // console.log("req:",req.user._id);
+  // const debugResult = await Transaction.find(
+  //   {// фильтация (без полей - все)
+  //     type: typeTrAct,// owner: '61879723dcf34c690cabb318',
+  //     owner:req.user._id,
 
-      date: { $gte: year, $lt: toDate }, // за указанный в запросе год, вплоть до 31дек
-    },
-    {
-      //список полей, забираемых из БД
-      _id: 0,
-      date: 1,
-      category: 1,
-      description: 1,
-      type: 1,
-      amount: 1,
-      owner: 1,
-    },
-    // {date:1}
-  ).sort({ date: 1 });
-  sendSuccessRes(res, { debugResult });
+  //     date: { $gte: year, $lt: toDate }, // за указанный в запросе год, вплоть до 31дек
+  //   },
+  //   {//список полей, забираемых из БД
+  //     _id: 0,
+  //     date: 1,
+  //     category: 1,
+  //     description: 1,
+  //     type: 1,
+  //     amount: 1,
+  //     owner: 1,
+  //   },// {date:1}
+  // ).sort({ date: 1 });
+  // sendSuccessRes(res, { debugResult });
   // ----------- end debug block-----------------
 
-  // sendSuccessRes(res, { sumsByMonth: sums });
+  sendSuccessRes(res, { sumsByMonth: sums });
 };
 
 module.exports = getYearByMonthAndType;
