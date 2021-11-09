39adcd9073e262d1086166dfd291ab6018b00da1
Shuydo <shuydo12@gmail.com>
2021-11-09 17:09:16 +0200

    response array by request yyyymm work

--- controllers/expenses/monthDetalReport.js
+++ controllers/expenses/monthDetalReport.js
@@ -1,71 +1,58 @@
-// for ex: http://localhost:3000/api/expenses?i=202101(YYYYMM)
+// for ex: http://localhost:3000/api/expenses?202101(YYYYMM)
 
 const { sendSuccessRes } = require('../../helpers');
-const { Transaction, Category } = require('../../models');
+const { Transaction } = require('../../models');
 
 const monthDetalReport = async (req, res) => {
-  console.log('in monthDetalReport');
+  // console.log('in monthDetalReport');
+  const query = Object.keys(req.query)[0];
+  const year = query.substr(0, 4);
+  const month = query[4] + query[5];
+  const fromDate = new Date(year + ' ' + month + 'UTC');
 
-  const key = Object.keys(req.query)[0].toLowerCase();
-  let typeTrAct; // получение типа искомых тАкций из запроса
-  if (key === 'i' || key === 'o') typeTrAct = key === 'i' ? true : false;
-  else console.log('Error type !!!'); // if (typeTrAct) console.log('Доход');  else console.log('Расход');
+  const monthN = +month + 1 === 13 ? 1 : +month + 1;
+  const yearN = +month + 1 === 13 ? +year + 1 : +year;
+  const toDate = new Date(yearN.toString() + ' ' + monthN.toString() + 'UTC');
 
-  const query = Object.values(req.query)[0];
-  // console.log("query:",query);
-  const yyyymm = query.substr(0, 6);
-  // console.log("yyyymm:",yyyymm);
-  const year = yyyymm.substr(0, 4); // получение 4х цифр года из запроса, с отбрасыванием остального
-  const month = yyyymm[4] + yyyymm[5];
-  // console.log("year,month:",year,month);
-
-  const result = await Category.find({}, { name: 1 });
-  
-  console.log(result);
-
-  // const toDate = year + '-12-31';
-  // const result = await Transaction.find(
-  //   {
-  //     type: typeTrAct,
-  //     owner:req.user._id,
-  //     date: { $gte: year, $lt: toDate },
-  //   },
-  //   {
-  //     _id: 0,
-  //     date: 1,
-  //     amount: 1,
-  //   },
-  // ).sort({ date: 1 });
-
-  // const sums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  // result.forEach(tAction => (sums[tAction.date.getMonth()] += tAction.amount));
-
-  // ============ debug block =================
-  // удалить и раскоментировать строку за ним
-  // sums.forEach((monthSum, monthNum) => console.log(monthNum, '-', monthSum));
-  // console.log("req:",req.user._id);
-  const debugResult = await Transaction.find(
+  const result = await Transaction.find(
     {
-      // фильтация (без полей - все)
-      type: typeTrAct, // owner: '61879723dcf34c690cabb318',
       owner: req.user._id,
-      // date: { $gte: year, $lt: toDate }, // за указанный в запросе год, вплоть до 31дек
+      date: { $gte: fromDate, $lt: toDate },
     },
     {
-      //список полей, забираемых из БД
       _id: 0,
-      date: 1,
-      category: 1,
-      description: 1,
-      type: 1,
-      amount: 1,
-      owner: 1,
-    }, // {date:1}
+      createdAt: 0,
+      updatedAt: 0,
+    },
   ).sort({ date: 1 });
-  sendSuccessRes(res, { debugResult });
+  // ============ debug block =================
+  // удалить и раскоментировать строку за ним
+  // const debugResult = await Transaction.find(
+  //   {
+  //     // фильтация (без полей - все)
+  //     // type: typeTrAct, // owner: '61879723dcf34c690cabb318',
+  //     owner: req.user._id,
+  //     date: { $gte: fromDate, $lt: toDate }, 
+  //     // date: { $gt: fromDate },
+  //   },
+  //   {
+  //     //список полей, забираемых из БД
+  //     _id: 0,
+  //     // date: 1,
+  //     // category: 1,
+  //     // description: 1,
+  //     // type: 1,
+  //     // amount: 1,
+  //     // owner: 1,
+  //     createdAt: 0,
+  //     updatedAt: 0,
+  //   }, // {date:1}
+  // ).sort({ date: 1 });
+  // // console.log("dr0:",debugResult[0]);
+  // sendSuccessRes(res, { tActionsMonth: debugResult });
   // ----------- end debug block-----------------
 
-  // sendSuccessRes(res, { month });
+  sendSuccessRes(res, { tActionsMonth: result });
 };
 
 module.exports = monthDetalReport;
