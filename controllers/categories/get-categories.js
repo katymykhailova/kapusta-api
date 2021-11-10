bd11920122db069f7fd9c0816bd9841adf849511
Kateryna Mykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-11-10 13:01:23 +0200

    update get-categories

--- controllers/categories/get-categories.js
+++ controllers/categories/get-categories.js
@@ -2,7 +2,6 @@ const { sendSuccessRes } = require('../../helpers');
 const { Category } = require('../../models');
 
 const categories = async (req, res) => {
-  // const { _id } = req.user;
   const result = await Category.find({}, '_id name');
   sendSuccessRes(res, { result });
 };
