1f82ccc2f13466b19ab7b20f6aadc9a2fdd98b8c
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-11-14 19:51:33 +0200

    upd googleauth

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -72,8 +72,8 @@ const googleRedirect = async (req, res) => {
   const { _id } = user;
   const payload = { _id };
   const token = jwt.sign(payload, SECRET_KEY);
-  const userToken = await User.findByIdAndUpdate(_id, { token });
-  console.log(userToken);
+  await User.findByIdAndUpdate(_id, { token });
+  const userToken = await User.findOne({ token });
   res.redirect(`${process.env.FRONTEND_URL}?token=${userToken.token}`);
 };
 
