41542e32b64fbeba15eaf135c0ec5addf76460f3
Kateryna Mykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-11-17 12:13:06 +0200

    fix 7

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(cors({
   origin: '*'
 }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", *); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(express.json());
