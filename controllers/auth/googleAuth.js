86df288af33a632d5c8c7a7beb9cdb4c1f772fe3
Kateryna Mykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-11-17 13:15:48 +0200

    8

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -19,6 +19,7 @@ const googleAuth = async (req, res) => {
     access_type: 'offline',
     prompt: 'consent',
   });
+  res.header('Access-Control-Allow-Origin', '*');
   return res.redirect(
     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`,
   );
@@ -74,6 +75,7 @@ const googleRedirect = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY);
   await User.findByIdAndUpdate(_id, { token });
   const userToken = await User.findOne({ token });
+  res.header('Access-Control-Allow-Origin', '*');
   res.redirect(`${process.env.FRONTEND_URL}?token=${userToken.token}`);
 };
 
