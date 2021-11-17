84f331328caff115fd314a540dcfdb47a578d94b
Kateryna Mykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-11-17 12:10:29 +0200

    header

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(logger(formatsLogger));
 app.use(cors({
   origin: '*'
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", *); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(express.static('public'));
 
